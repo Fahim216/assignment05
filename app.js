5e7bcdd793d4d2017008d98b46432a414972afef
Fahim216 <73400870+Fahim216@users.noreply.github.com>
2021-02-09 15:46:15 +0600

    updated

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 const searchfood=()=>{
     const searchText=document.getElementById('search-field').value;
     const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    //load data
     fetch(url)
     .then(res=>res.json())
     .then(data=>displayFood(data.meals))
 }
 
-
-const displayFood=food=>{
+const displayFood=foods=>{
     const foodContainer=document.getElementById('food-container');
 
+    foods.forEach(food=>{
+       
+        const foodDiv=document.createElement('div');
+        foodDiv.innerHTML=`
+        <div class="col-md-4 food-item">
+        <img src="${food.strMealThumb}" class="food-image">
+        <h3 class="food-name">${food.strMeal}</h3>
+        <ul><li>
+        <p class="food-details">${food.strIngredient1}</p></li>
+        <li> <p class="food-details">${food.strIngredient2}</p> </li>
+        <li><p class="food-details">${food.strIngredient3}</p></li>
+        <li><p class="food-details">${food.strIngredient4}</p></li>
+        <li><p class="food-details">${food.strIngredient5}</p></li>
+        <li><p class="food-details">${food.strIngredient6}</p></li>
+        </ul>
+        
+        
+        `
+        foodContainer.appendChild(foodDiv);
 
-   food.forEach(food=> {
-       const foodDiv=document.createElement('div');
-       foodDiv.className='single-result row align-items-center my-3 p-3';
-       foodDiv.innerHTML=`
-       <div class="col-md-9">
-       <h3 class="lyrics-name">purple</h3>
-       <p class="author lead">Album</p>
-       </div>
-       <div class="col-md-3 text-md-right text-center">
-       <button class="btn btn-success">Get Lyrics</button>
-       </div>
-
-       `
-       foodContainer.appendChild(foodDiv);
-   })
+    })
 }
+
